7cc36824968f5958e29ede45b89617505746b39c
Valeria <montrucchio.valeria@gmail.com>
2014-08-20 23:21:25 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+function hoverOver(button) {
+  button.innerHTML="Thank You"
+}
+
+function unHover(button) {
+  button.innerHTML="sign up"
+}
+
+/*function clickDown(button) {
+  document.getElementById("demo").input.innerHTML="thank you"
+}
+
+function unClick(button){
+  document.getElementById("demo").input.innerHTML="sign up"
+}
+
+function myFunction(input) {
+    document.getElementById("myForm").innerHTML= “submit valid email”*/
+
+
+
+
+
+
+    /*TRIED but DOESN’T WORK
+<form>
+            <input type="text" onclick="myFunction()" id="myForm" value="email"/>
+            <button type="submit"> Sign Up</button>
+        </form>
+
+        <script>
+function myFunction() {
+    document.getElementById("myForm").innerHTML= “submit valid email”;
+}
+</script>
+
+
+
+DIDN’T WORK
+inner, when click it appears a new text
+
+HTML
+<input type="text" value="email" onmousedown="clickDown(this)" onmouseup="clickUp(this)" />
+
+JS
+function clickDown(input) {
+  input.innerHTML="please write a valid email"
+}
+
+function clickUp(input){
+  input.innerHTML="email"*/
+}
---
+++ stylesheets/app.js
@@ -0,0 +1,18 @@
+function hoverOver(button) {
+  button.innerHTML="Thank You"
+}
+
+function unHover(button) {
+  button.innerHTML="sign up"
+}
+
+function clickDown(button) {
+  document.getElementById("demo").input.innerHTML="thank you"
+}
+
+function unClick(button){
+  document.getElementById("demo").input.innerHTML="sign up"
+}
+
+function myFunction(input) {
+    document.getElementById("myForm").innerHTML= “submit valid email”
